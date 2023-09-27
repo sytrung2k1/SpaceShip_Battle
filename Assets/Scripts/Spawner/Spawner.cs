3c80595137b842aef04c65cd343aa972d0e0990c
sytrung2k1 <sytrung2k1@gmail.com>
2023-09-27 11:44:08 +0700

    apply object pooling to bullets

--- Assets/Scripts/Spawner/Spawner.cs
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public abstract class Spawner : MyUpdateMonoBehaviour
 {
+    [SerializeField] protected Transform holder;
     [SerializeField] protected List<Transform> prefabs;
+    [SerializeField] protected List<Transform> poolObjs;
 
     protected override void LoadComponents()
     {
-        base.LoadComponents();
         this.LoadPrefabs();
+        this.LoadHolder();
+    }
+
+    protected virtual void LoadHolder()
+    {
+        if (this.holder != null) return;
+        this.holder = transform.Find("Holder");
+        Debug.Log(transform.name + ": Load Holder", gameObject);
     }
 
     protected virtual void LoadPrefabs()
@@ -44,8 +53,33 @@ public abstract class Spawner : MyUpdateMonoBehaviour
             return null;
         }
 
-        Transform newPrefab = Instantiate(prefab, spawnPos, rotation);
+        Transform newPrefab = this.GetObjectFromPool(prefab);
+        newPrefab.SetPositionAndRotation(spawnPos, rotation);
+
+        newPrefab.parent = this.holder;
         return newPrefab;
+    }
+
+    protected virtual Transform GetObjectFromPool(Transform prefab)
+    {
+        foreach(Transform poolObj in this.poolObjs)
+        {
+            if(poolObj.name == prefab.name)
+            {
+                this.poolObjs.Remove(poolObj);
+                return poolObj;
+            }
+        }
+
+        Transform newPrefab = Instantiate(prefab);
+        newPrefab.name = prefab.name;
+        return newPrefab;
+    }
+
+    public virtual void Despawn(Transform obj)
+    {
+        this.poolObjs.Add(obj);
+        obj.gameObject.SetActive(false);
     }
 
     public virtual Transform GetPrefabByName(string prefabName)
